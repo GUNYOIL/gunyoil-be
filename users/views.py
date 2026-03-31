e78ce228255c2f7ab09b66bc24d0e813b73d8be6
imhanseong <hans.hanseong@gmail.com>
2026-03-31 12:24:38 +0900

    feat: implement custom user model and JWT authentication API

---
+++ users/views.py
@@ -0,0 +1,44 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class SignupView(APIView):
+    permission_classes = [AllowAny] 
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "회원가입 성공"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class OnboardingCompleteView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        user = request.user
+        user.onboarding_completed = True
+        user.save()
+        return Response({"message": "온보딩이 완료되었습니다.", "onboarding_completed": True}, status=status.HTTP_200_OK)
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CustomTokenObtainPairSerializer
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
