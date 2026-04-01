2420d389ac154bf4464d83e366c64a163cdd95d2
imhanseong <hans.hanseong@gmail.com>
2026-04-01 15:07:38 +0900

    feat: add workout today routes

--- workouts/urls.py
+++ workouts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import WorkoutHistoryView
+from .views import WorkoutHistoryView, TodayWorkoutView, WorkoutSetUpdateView
 
 urlpatterns = [
-    # 잔디 데이터를 가져올 주소: GET /me/workouts/history/
     path('history/', WorkoutHistoryView.as_view(), name='workout_history'),
+    path('today/', TodayWorkoutView.as_view(), name='workout_today'),
+    path('sets/<int:set_id>/', WorkoutSetUpdateView.as_view(), name='workout_set_update'),
 ]
