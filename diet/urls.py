4c5ea02c9c0872bf84db32be941c7ad24d3709c8
imhanseong <hans.hanseong@gmail.com>
2026-04-01 15:18:10 +0900

    feat: add meal log routes

--- diet/urls.py
+++ diet/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 
-from .views import ProteinLogCreateView, ProteinLogDeleteView, ProteinView
+from .views import (
+    MealLogCreateView,
+    MealLogDeleteView,
+    MealView,
+    ProteinLogCreateView,
+    ProteinLogDeleteView,
+    ProteinView,
+)
 
 
 urlpatterns = [
     path('protein', ProteinView.as_view(), name='protein_overview'),
     path('protein/logs', ProteinLogCreateView.as_view(), name='protein_log_create'),
     path('protein/logs/<int:log_id>/', ProteinLogDeleteView.as_view(), name='protein_log_delete'),
+    path('meals', MealView.as_view(), name='meal_overview'),
+    path('meals/logs', MealLogCreateView.as_view(), name='meal_log_create'),
+    path('meals/logs/<int:meal_id>/', MealLogDeleteView.as_view(), name='meal_log_delete'),
 ]
