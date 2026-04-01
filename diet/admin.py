367fb881411b356c437d38529f7d6040a9c7db85
imhanseong <hans.hanseong@gmail.com>
2026-04-01 15:18:10 +0900

    feat: register meal log admin

--- diet/admin.py
+++ diet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import ProteinLog
+from .models import MealLog, ProteinLog
 
 
 @admin.register(ProteinLog)
@@ -8,3 +8,10 @@ class ProteinLogAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'date', 'amount', 'log_type', 'created_at']
     list_filter = ['log_type', 'date']
     search_fields = ['user__email', 'note']
+
+
+@admin.register(MealLog)
+class MealLogAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'date', 'meal_type', 'name', 'calories', 'created_at']
+    list_filter = ['meal_type', 'date']
+    search_fields = ['user__email', 'name', 'memo']
