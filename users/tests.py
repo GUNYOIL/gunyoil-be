1636972076f1cc5274888e09c321474d4d853544
imhanseong <hans.hanseong@gmail.com>
2026-04-01 15:45:38 +0900

    test: add onboarding draft coverage

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+User = get_user_model()
+
+
+class UserApiTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email='user@example.com',
+            password='password123',
+            onboarding_completed=True,
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_save_onboarding_draft(self):
+        response = self.client.put(
+            reverse('onboarding_draft'),
+            {
+                'gender': 'M',
+                'height': 180,
+                'weight': 78,
+                'goal': 'muscle gain',
+            },
+            format='json',
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.height, 180)
+        self.assertEqual(self.user.weight, 78)
+        self.assertFalse(self.user.onboarding_completed)
