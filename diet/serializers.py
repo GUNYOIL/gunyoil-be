457adcdc9283363e6e61dfc4ef1434ab6f8ae0bf
imhanseong <hans.hanseong@gmail.com>
2026-04-01 15:18:10 +0900

    feat: add meal log serializers

--- diet/serializers.py
+++ diet/serializers.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 from django.utils import timezone
 from rest_framework import serializers
 
-from .models import ProteinLog
+from .models import MealLog, ProteinLog
 
 
 class ProteinLogSerializer(serializers.ModelSerializer):
@@ -42,3 +42,66 @@ class ProteinOverviewSerializer(serializers.Serializer):
     progress_percent = serializers.IntegerField()
     is_target_completed = serializers.BooleanField()
     logs = ProteinLogSerializer(many=True)
+
+
+class MealLogSerializer(serializers.ModelSerializer):
+    type = serializers.CharField(source='meal_type')
+    type_label = serializers.CharField(source='get_meal_type_display', read_only=True)
+
+    class Meta:
+        model = MealLog
+        fields = [
+            'id',
+            'date',
+            'type',
+            'type_label',
+            'name',
+            'calories',
+            'protein',
+            'carbs',
+            'fat',
+            'memo',
+            'created_at',
+        ]
+        read_only_fields = ['id', 'created_at']
+
+
+class MealLogCreateSerializer(serializers.ModelSerializer):
+    type = serializers.ChoiceField(choices=MealLog.MealType.choices, source='meal_type')
+    date = serializers.DateField(required=False)
+
+    class Meta:
+        model = MealLog
+        fields = ['date', 'type', 'name', 'calories', 'protein', 'carbs', 'fat', 'memo']
+
+    def validate_calories(self, value):
+        if value < 0:
+            raise serializers.ValidationError('calories must be 0 or greater.')
+        return value
+
+    def _validate_non_negative_decimal(self, value, field_name):
+        if value < Decimal('0'):
+            raise serializers.ValidationError(f'{field_name} must be 0 or greater.')
+        return value
+
+    def validate_protein(self, value):
+        return self._validate_non_negative_decimal(value, 'protein')
+
+    def validate_carbs(self, value):
+        return self._validate_non_negative_decimal(value, 'carbs')
+
+    def validate_fat(self, value):
+        return self._validate_non_negative_decimal(value, 'fat')
+
+    def create(self, validated_data):
+        validated_data.setdefault('date', timezone.localdate())
+        return MealLog.objects.create(**validated_data)
+
+
+class MealOverviewSerializer(serializers.Serializer):
+    date = serializers.DateField()
+    total_calories = serializers.IntegerField()
+    total_protein = serializers.DecimalField(max_digits=6, decimal_places=1)
+    total_carbs = serializers.DecimalField(max_digits=6, decimal_places=1)
+    total_fat = serializers.DecimalField(max_digits=6, decimal_places=1)
+    meals = MealLogSerializer(many=True)
