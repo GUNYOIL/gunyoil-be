7fbdb888ea5c66bb27377c1e28189468c3c472d9
imhanseong <hans.hanseong@gmail.com>
2026-04-01 15:07:37 +0900

    feat: add workout today serializers

--- workouts/serializers.py
+++ workouts/serializers.py
@@ -1,5 +1,19 @@
 from rest_framework import serializers
-from .models import DailyLog
+from .models import DailyLog, WorkoutSet
+
+class WorkoutSetSerializer(serializers.ModelSerializer):
+    exercise_name = serializers.CharField(source='exercise.name', read_only=True)
+
+    class Meta:
+        model = WorkoutSet
+        fields = ['id', 'exercise', 'exercise_name', 'set_number', 'weight', 'reps', 'is_completed']
+
+class TodayLogSerializer(serializers.ModelSerializer):
+    sets = WorkoutSetSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = DailyLog
+        fields = ['id', 'date', 'is_completed', 'sets']
 
 class DailyLogSerializer(serializers.ModelSerializer):
     class Meta:
