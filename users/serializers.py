50c72d7f20386c92a0483e3fead2de0de1d4198e
imhanseong <hans.hanseong@gmail.com>
2026-04-01 16:11:19 +0900

    feat: add user dashboard serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,10 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 
 User = get_user_model()
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,16 +20,50 @@ class UserSerializer(serializers.ModelSerializer):
             height=validated_data.get('height'),
             weight=validated_data.get('weight'),
             goal=validated_data.get('goal'),
-            onboarding_completed=validated_data.get('onboarding_completed', False)
+            onboarding_completed=validated_data.get('onboarding_completed', False),
         )
         return user
 
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class PasswordChangeSerializer(serializers.Serializer):
+    current_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True, min_length=8)
+
+    def validate_current_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError('Current password is incorrect.')
+        return value
+
+
+class GrassEntrySerializer(serializers.Serializer):
+    date = serializers.DateField()
+    is_completed = serializers.BooleanField()
+
+
+class RecentWorkoutSerializer(serializers.Serializer):
+    date = serializers.DateField()
+    is_completed = serializers.BooleanField()
+
+
+class DashboardProteinSerializer(serializers.Serializer):
+    target_amount = serializers.DecimalField(max_digits=6, decimal_places=1, allow_null=True)
+    consumed_amount = serializers.DecimalField(max_digits=6, decimal_places=1)
+    progress_percent = serializers.IntegerField()
+    is_target_completed = serializers.BooleanField()
+
+
+class DashboardSerializer(serializers.Serializer):
+    date = serializers.DateField()
+    today_workout = serializers.DictField()
+    protein = DashboardProteinSerializer()
+    recent_workouts = RecentWorkoutSerializer(many=True)
+
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
         return {
             'access': data['access'],
-            'refresh': data['refresh']
+            'refresh': data['refresh'],
         }
