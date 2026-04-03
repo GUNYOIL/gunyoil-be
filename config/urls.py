61691fd77fcf722d0f05bc76784096530d2bfcf9
imhanseong <hans.hanseong@gmail.com>
2026-04-03 13:37:40 +0900

    feat: add swagger routes

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('', include('users.urls')),
     path('catalog/', include('exercises.urls')),
     path('me/', include('routines.urls')),
