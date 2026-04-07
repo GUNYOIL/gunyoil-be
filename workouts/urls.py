df796d5c8ef0371590abc682f535c030adb4c391
imhanseong <hans.hanseong@gmail.com>
2026-04-07 12:18:13 +0900

    fix: add trailing slash support for workout set route

--- workouts/urls.py
+++ workouts/urls.py
@@ -6,5 +6,6 @@ from .views import TodayWorkoutSetCreateView, TodayWorkoutView, WorkoutHistoryVi
 urlpatterns = [
     path('history/', WorkoutHistoryView.as_view(), name='workout_history'),
     path('today/', TodayWorkoutView.as_view(), name='workout_today'),
-    path('today/sets', TodayWorkoutSetCreateView.as_view(), name='workout_set_update'),
+    path('today/sets/', TodayWorkoutSetCreateView.as_view(), name='workout_set_update'),
+    path('today/sets', TodayWorkoutSetCreateView.as_view()),
 ]
