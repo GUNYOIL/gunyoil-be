15bdff813592b8d950b0dbe3022ff111509ceb62
imhanseong <hans.hanseong@gmail.com>
2026-04-07 12:12:18 +0900

    feat: improve exercise admin

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Exercise
 
-admin.site.register(Exercise)
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'category', 'target_muscle', 'is_active')
+    list_filter = ('category', 'is_active')
+    search_fields = ('name', 'code', 'target_muscle')
