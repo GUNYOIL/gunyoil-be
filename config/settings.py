f8aaa9d367fbe912d2c65e13a8f183e1d170795d
imhanseong <hans.hanseong@gmail.com>
2026-04-08 20:07:30 +0900

    chore: Vercel 어드민 도메인 CORS 허용 추가

--- config/settings.py
+++ config/settings.py
@@ -171,7 +171,10 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'http://localhost:3001',
     'https://gunyoil.vercel.app',
+    'https://gunyoil-admin.vercel.app',
+    'https://gunyoil-admin.vercel.app/',
 ]
 
 NEIS_API_KEY = os.getenv('NEIS_API_KEY')
