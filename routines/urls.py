4d62324379c81dd4088b6996b6967e1606ac7b48
imhanseong <hans.hanseong@gmail.com>
2026-04-08 11:35:02 +0900

    fix: support slashless routine routes

--- routines/urls.py
+++ routines/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from .views import UserRoutineView
 
+
 urlpatterns = [
-    # 주소: /me/routines/
+    path('routines', UserRoutineView.as_view(), name='user_routines_no_slash'),
     path('routines/', UserRoutineView.as_view(), name='user_routines'),
 ]
