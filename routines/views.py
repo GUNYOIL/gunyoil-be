d89fe1bf534f9c93d1ab117298dafd406739f038
imhanseong <hans.hanseong@gmail.com>
2026-06-27 21:47:10 +0900

    refactor: wrap routine updates in atomic transaction

--- routines/views.py
+++ routines/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
@@ -21,22 +22,23 @@ class UserRoutineView(APIView):
         if not isinstance(data, list):
             return error_response('리스트([]) 형태로 데이터를 보내주세요.', status_code=status.HTTP_400_BAD_REQUEST)
 
-        Routine.objects.filter(user=request.user).delete()
+        with transaction.atomic():
+            Routine.objects.filter(user=request.user).delete()
 
-        for routine_data in data:
-            day = routine_data.get('day_of_week')
-            routine = Routine.objects.create(user=request.user, day_of_week=day)
+            for routine_data in data:
+                day = routine_data.get('day_of_week')
+                routine = Routine.objects.create(user=request.user, day_of_week=day)
 
-            details_data = routine_data.get('details', [])
-            for detail in details_data:
-                RoutineDetail.objects.create(
-                    routine=routine,
-                    exercise_id=detail['exercise'],
-                    target_weight=detail.get('target_weight', 0),
-                    target_reps=detail.get('target_reps', 0),
-                    target_sets=detail.get('target_sets', 0),
-                    order=detail.get('order', 0),
-                )
+                details_data = routine_data.get('details', [])
+                for detail in details_data:
+                    RoutineDetail.objects.create(
+                        routine=routine,
+                        exercise_id=detail['exercise'],
+                        target_weight=detail.get('target_weight', 0),
+                        target_reps=detail.get('target_reps', 0),
+                        target_sets=detail.get('target_sets', 0),
+                        order=detail.get('order', 0),
+                    )
 
         new_routines = Routine.objects.filter(user=request.user).order_by('day_of_week')
         serializer = RoutineSerializer(new_routines, many=True)
